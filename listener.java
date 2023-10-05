5da845bf573a8b87a759c629175d8279910a1d64
SamuelCano03 <102400537+SamuelCano03@users.noreply.github.com>
2023-10-04 20:28:24 -0500

    Add files via upload

---
+++ listener.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+
+public class listener extends graphizBaseListener{
+    public Map<String, List<Object[]>> tupleMap = new HashMap<>();
+    public void agregar(String clave, Object[] valor) {
+        // Verificamos si la clave ya existe en el mapa
+        if (tupleMap.containsKey(clave)) {
+            // Si la clave ya existe, obtenemos la lista actual y le añadimos el valor
+            List<Object[]> listaActual = tupleMap.get(clave);
+            listaActual.add(valor);
+        } else {
+            // Si la clave no existe, creamos una nueva lista y añadimos el valor
+            List<Object[]> nuevaLista = new ArrayList<>();
+            nuevaLista.add(valor);
+            tupleMap.put(clave, nuevaLista);
+        }
+    }
+    /* edge_stmt */
+    @Override
+    public void enterEdge_stmt(graphizParser.Edge_stmtContext ctx){
+        
+        String src = ctx.node_id().ID().getText();
+        String edge = ctx.edgeRHS().node_id().getText();
+        String weight;
+        if(ctx.attr_list() != null && ctx.attr_list().a_list() !=null){
+            weight=ctx.attr_list().a_list().id(1).getText();
+        }
+        else{
+            weight="0";
+        }
+        agregar(src, new Object []{edge, weight});
+
+    }
+
+}
